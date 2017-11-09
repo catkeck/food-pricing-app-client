2518f281ab9bdb051fb921aaabae2073a1f4ebe9
Caitlin Keck <caitlin.keck@gmail.com>
2017-11-08 21:03:49 -0500

    removed automated formatting for header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,19 +11,12 @@ import {
   function handleLogout() {
     localStorage.removeItem("token")
   }                      
-  return (                        
-    <Menu fixed='top' size='large'>
-      <Container>
-        <Menu.Menu position='left'>
-          <Menu.Item as='a'> <Link to={`/`}>ButterGrams</Link></Menu.Item>
-          {localStorage["token"] ? <Menu.Item as='a'> <Link to={`/user/recipes`}>Saved Recipes</Link></Menu.Item> : null}
-        </Menu.Menu>
-          <h1><Link to={`/`}>Butter Grams</Link></h1>
-        <Menu.Menu position='right'>
-          {localStorage["token"] ? <Menu.Item as='a'><Link to={'/'} onClick={handleLogout}> Log Out</Link></Menu.Item> : <div><Menu.Item as='a'><Link to={'/signup'}>Sign Up</Link></Menu.Item><Menu.Item as='a'> <Link to={'/login'}>Log In</Link></Menu.Item></div>}
-        </Menu.Menu>
-      </Container>
-    </Menu>
+  return (
+    <div className = "navbar">                        
+      <h2><Link to={`/`}>Butter Grams</Link></h2>
+      {localStorage["token"] ? <Link to={`/user/recipes`}>Saved Recipes</Link> : null}
+      {localStorage["token"] ? <Link to={'/'} onClick={handleLogout}> Log Out</Link>: <div><Link to={'/signup'}>Sign Up</Link><Link to={'/login'}>Log In</Link></div>}
+    </div>
   )
 }
     
