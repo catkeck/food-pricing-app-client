5f79824d3f57afef702f84c16563a7af7719126b
Caitlin Keck <caitlin.keck@gmail.com>
2017-11-22 17:07:53 -0500

    fooling around with key

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Restaurant = props => {
+  console.log(props);
   return (
     <div className="restaurant">
       <Link to={`/menus/${props.data.id}`}>
         <li>
-          <span>{props.data.key}.</span>
-          <h4> {props.data.name}</h4>
+          <span>{props.key}.</span>
+          {props.data.name}
           <p>{props.data.location.address}</p>
           <p>{props.data.url}</p>
         </li>
