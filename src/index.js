ae2f5c9b4eea4e82df439e3971a4b9e2da9250b3
Caitlin Keck <caitlin.keck@gmail.com>
2017-11-22 15:41:43 -0500

    removed semantic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+// import 'semantic-ui-css/semantic.min.css';
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(<Router><Route path="/" component={App} /></Router>, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Route path="/" component={App} />
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
