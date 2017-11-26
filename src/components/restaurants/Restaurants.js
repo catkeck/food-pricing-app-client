0e84de53df292e1c9f93202b9b7e848a605afe09
Caitlin Keck <caitlin.keck@gmail.com>
2017-11-25 19:03:41 -0500

    formatted list of restaurants further and changed structure of component

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -3,14 +3,16 @@ import Restaurant from "./Restaurant";
 
 const Restaurants = props => {
   return (
-    <div className="restaurants">
-      {props.data.venues
-        ? props.data.venues.map((venue, index) => (
+    <div>
+      {props.data.venues ? (
+        <div className="restaurants">
+          {props.data.venues.map((venue, index) => (
             <ol>
               <Restaurant key={index} data={venue} showMenu={props.showMenu} />
             </ol>
-          ))
-        : null}
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 };
